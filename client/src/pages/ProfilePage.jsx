b1b9be91aadd098afb2f49c0bf8b53d455e838aa
aniket00101 <dasaniket971@gmail.com>
2025-08-15 17:34:29 +0530

    changes

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -30,7 +30,7 @@ const ProfilePage = () => {
         <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-10 flex-1">
           <h3 className="text-lg">Profile details</h3>
           <label htmlFor="avatar" className="flex items-center gap-3 cursor-pointer">
-            <input onChange={(e) => setSelectedImg(e.target.files[0])} type="file" id="avatar" accept=".png, .jpg, .jpeg" hidden />
+            <input onChange={(e) => setSelectedImg(e.target.files[0])} type="file" id="avatar" accept="*/*" hidden />
             <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'}`} />upload profile image
           </label>
           <input onChange={(e) => setName(e.target.value)} value={name} type="text" required placeholder="Your name" className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" />
