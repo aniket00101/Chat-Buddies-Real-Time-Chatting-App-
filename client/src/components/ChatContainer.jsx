589fc6e047e9e3785130ae011141557437921349
aniket00101 <dasaniket971@gmail.com>
2025-08-15 17:58:03 +0530

    mini changes

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -20,13 +20,13 @@ const ChatContainer = () => {
 
   const handleSendImage = async (e) => {
     const file = e.target.files[0];
-    if(!file || !file.type.startWith("image/")){
+    if(!file || !file.type.startsWith("image/")){
       toast.error("Select an image file");
       return;
     }
     const reader = new FileReader();
     reader.onloadend = async () => {
-      await sendMessage({image: reader.result})
+      await sendMessage({ image: reader.result })
       e.target.value = ""
     }
     reader.readAsDataURL(file)
@@ -73,7 +73,7 @@ const ChatContainer = () => {
       <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
         <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
           <input onChange={(e)=> setInput(e.target.value)} value={input} onKeyDown={(e)=> e.key === "Enter" ? handleSendMessage(e) : null} type="text" placeholder="Send a message" className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400" />
-          <input onChange={handleSendImage} type="file" id="image" accept="image/png, image,jeg" hidden />
+          <input onChange={handleSendImage} type="file" id="image" accept="image/*" hidden />
           <label htmlFor="image">
             <img src={assets.gallery_icon} alt="" className="w-5 mr-2 cursor-pointer" />
           </label>
